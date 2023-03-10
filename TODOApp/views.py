92d833eeb65f6443c89e975c1c9624ce1356056f
Tonny Oseko <tonnyoseko0@gmail.com>
2023-03-10 23:41:54 +0300

    Create a view to list todo items

--- TODOApp/views.py
+++ TODOApp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from TODOApp.models import TodoItem
+from TODOApp.serializers import TodoItemSerializer
+from rest_framework import generics
+
 
 # Create your views here.
+
+class TodoItemView(generics.ListCreateAPIView):
+    queryset = TodoItem.objects.all()
+    serializer_class = TodoItemSerializer
