a091cbba2ac51e8b269851db4d3524d606673fb7
Tonny Oseko <tonnyoseko0@gmail.com>
2023-03-13 14:57:46 +0300

    create authentication views for the user

--- TODOApp/views.py
+++ TODOApp/views.py
@@ -1,6 +1,28 @@
 from rest_framework import generics, permissions
 from .models import TodoItem
 from .serializers import TodoItemSerializer
+from django.contrib.auth.models import User
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from .serializers import UserSerializer
+
+
+# create authentification views
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    model = User
+    permission_classes = [permissions.AllowAny]
+    serializer_class = UserSerializer
+
 
 # Create your views here.
 
@@ -15,3 +37,12 @@ class TodoItemList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+
+class TodoItemDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = TodoItem.objects.all()
+    serializer_class = TodoItemSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return TodoItem.objects.filter(owner=self.request.user)
